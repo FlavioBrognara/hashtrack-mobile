962eead0301e4c4a1b4525bf9dcc4317bf407f58
Kaic Bento <kaicbento@outlook.com>
2017-02-21 20:08:04 -0300

    .

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 
 app.set('port', process.env.PORT || 8000);
 
-app.use(express.compress());
 app.use(express.static('./client'));
 
 app.listen(app.get('port'), function () {});
