e8c3cfc7c9751ed735c0bb63fdb7bb207ccea82b
Kaic Bento <kaicbento@outlook.com>
2017-02-21 19:59:58 -0300

    changes

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 
 app.use(express.logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname + '/client'));////
+app.use(express.static(__dirname + '/client'));
 
-app.listen(app.get('port'), function() {
-});
+app.listen(app.get('port'), function () {});
