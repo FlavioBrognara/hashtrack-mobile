965ca722a56dd5fe1f823dbd7034e89845ccb296
Kaic Bento <kaicbento@outlook.com>
2017-02-23 16:48:22 -0300

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var https = require('https');
 var fs = require('fs');
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
 
 app.use(express.static('./client'));
