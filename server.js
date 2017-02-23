42558de4a055ff27ed9c091ae4c2e72e71038e49
Kaic Bento <kaicbento@outlook.com>
2017-02-23 17:05:49 -0300

    .

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var app = express();
 
 app.set('port', process.env.PORT || 8000);
 
-app.set('trust proxy', 'loopback');
 app.use(express.static('./client'));
 
-app.listen(app.get('port'), function () {});
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+
+
+var options = {
+  key: fs.readFileSync('ssl/hashtrack.io.key'),
+  cert: fs.readFileSync('ssl/hashtrack.io.chained.crt'),
+  ca: fs.readFileSync('ssl/gd_bundle-g2-g1.crt')
+  
+};
+
+https.createServer(options, app).listen(process.env.PORT || 8000, function () {
+});
