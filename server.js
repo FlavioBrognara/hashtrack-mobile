6192f63b57b2f173d75d1ef126e1c6dfe3c9343d
Kaic Bento <kaicbento@outlook.com>
2017-02-23 16:51:22 -0300

    .

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 var express = require('express');
-var https = require('https');
-var fs = require('fs');
 var app = express();
 
-var port = process.env.PORT || 8000;
-
+app.set('port', process.env.PORT || 8000);
 
 app.use(express.static('./client'));
 
-var options = {
-    key: fs.readFileSync('ssl/hashtrack.io.key'),
-    cert: fs.readFileSync('ssl/hashtrack.io.chained.crt'),
-    ca: fs.readFileSync('ssl/gd_bundle-g2-g1.crt')
-
-};
-
-https.createServer(options, app).listen(port, function () {
-    console.log('HTTPS Server http://localhost:8000');
-});
+app.listen(app.get('port'), function () {});
