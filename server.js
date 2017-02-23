eb3df34db807f41a7b20a596e52f53158614593a
Kaic Bento <kaicbento@outlook.com>
2017-02-23 16:55:49 -0300

    .

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port', process.env.PORT || 8000);
 
+app.set('trust proxy', 'loopback');
 app.use(express.static('./client'));
 
 app.listen(app.get('port'), function () {});
